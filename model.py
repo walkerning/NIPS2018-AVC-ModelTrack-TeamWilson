86d586c864537c23e424a00a1c7e3f8b77c29130
Behar Veliqi <bveliqi@Behars-MacBook-Pro.local>
2018-06-21 15:13:38 +0200

    Added documentation to the mocked model

--- model.py
+++ model.py
@@ -1,4 +1,11 @@
 class MockModel:
+    """ 
+        This is just a mock.
+        For a submission, you would normally use a foolbox wrapped model here,
+        based on your framework of choice.
+        For more details, have a look here:
+        https://foolbox.readthedocs.io/en/latest/modules/models.html
+    """
 
     def channel_axis(self):
         return 1
