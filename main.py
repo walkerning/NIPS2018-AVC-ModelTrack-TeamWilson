89a31dfcbb3434446d02537cc7871d56429c9292
Jonas Rauber <git@jonasrauber.de>
2018-07-03 10:34:35 +0000

    use L2BasicIterativeAttack

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import foolbox
 from fmodel import create_fmodel
-from adversarial_vision_challenge import model_server, load_model, read_images, store_adversarial
+from adversarial_vision_challenge import load_model
+from adversarial_vision_challenge import read_images
+from adversarial_vision_challenge import store_adversarial
 
 
 def run_attack(model, image, label):
     criterion = foolbox.criteria.Misclassification()
-    attack = foolbox.attacks.IterativeGradientAttack(model, criterion)    
-    return attack(image, label, epsilons=35, steps=12)
+    attack = foolbox.attacks.L2BasicIterativeAttack(model, criterion)
+    return attack(image, label)
 
 
 def main():
     # instantiate blackbox and substitute model
     forward_model = load_model()
     backward_model = create_fmodel()
-    
-    # instantiate differntiable composite model (predictions from blackbox, gradients from substitute)
-    model = foolbox.models.CompositeModel(forward_model=forward_model, backward_model=backward_model)
+
+    # instantiate differntiable composite model
+    # (predictions from blackbox, gradients from substitute)
+    model = foolbox.models.CompositeModel(
+        forward_model=forward_model,
+        backward_model=backward_model)
 
     for (file_name, image, label) in read_images():
         adversarial = run_attack(model, image, label)
